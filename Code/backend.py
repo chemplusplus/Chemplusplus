9b0886e0267ba9d389d0665eaabc326bd28f9605
Makus_Frig <72632895+MakusFrig@users.noreply.github.com>
2021-07-19 15:34:59 -0700

    Update backend.py
    I added some more functions to the backend file

--- Code/backend.py
+++ Code/backend.py
@@ -27,7 +27,7 @@ print(type(temp[0]))
 for i in temp:
 	table[i['Cell'][2]] = i['Cell']
 	ELECTRON_CONFIG[i['Cell'][1]] = i['Cell'][5]
-print(table)
+
 
 class ec:
 	def __init__(self, ec):
@@ -100,3 +100,32 @@ def config(x):
 
 	x = element(name, x, br)
 	return x 
+
+
+
+def calc_units(pe, me, ge, e):
+	temp_pe, temp_me, temp_ge = pe.get(), me.get(), ge.get()
+	e = e.get()
+	grams_to_mols = 0
+	for i in table:
+		if i == e:
+			grams_to_mols = float(table[i][3])
+	if temp_pe != '':
+		temp_pe = float(temp_pe)
+		me.insert(0, temp_pe/(6.02*10**23))
+		ge.insert(0, (temp_pe/(6.02*10**23)*grams_to_mols))
+	elif temp_me != '':
+		temp_me = float(temp_me)
+		pe.insert(0, str(round(temp_me*(6.02),5))+"*10^23")
+		ge.insert(0, temp_me/grams_to_mols)
+
+	elif temp_ge != '':
+		temp_ge = float(temp_ge)
+		me.insert(0, temp_ge/grams_to_mols)
+		pe.insert(0, str(round((temp_ge/grams_to_mols)*6.02, 5))+"*10^23")
+
+def clear_values(pe, me, ge, e):
+	pe.delete(0, len(pe.get()))
+	me.delete(0, len(me.get()))
+	ge.delete(0, len(ge.get()))
+	e.delete(0, len(e.get()))
