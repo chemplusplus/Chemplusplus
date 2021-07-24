fc92a48e1a7984b090ca8d5e5c5e01cccc572852
Makus_Frig <72632895+MakusFrig@users.noreply.github.com>
2021-07-24 14:22:32 -0700

    Create structure.py
    I created the file that shows diagrams of molecules once extracted from pubchem

---
+++ Code/structure.py
@@ -0,0 +1,64 @@
+'''
+This is the structure grabber file
+'''
+
+from tkinter import *
+
+import time
+
+import backend
+
+import pubchem
+
+import os
+
+def create_struct(t):
+	global _Struct_Frame, _Struct_Entry, _Struct_Enter
+
+	_Struct_Frame = LabelFrame(t, text = 'Compound Diagram', width = 200, height = 80, font = ("Montserrat", 10), bg = "#373e40", fg = "#ffffff")
+
+	_Struct_Frame.place(x = 210, y = 100)
+
+	_Struct_Entry = Entry(_Struct_Frame,bg = '#ffffff', fg = '#121212', font = ("Montserrat", 10), width = 19)
+
+	_Struct_Entry.insert(0, "Compound Name")
+
+	_Struct_Enter = Button(_Struct_Frame, text = 'Get Struct', command = lambda:create_window(_Struct_Entry.get()), font = ('Montserrat', 10), width = 17)
+
+	_Struct_Enter.place(x = 5, y = 25)
+
+	_Struct_Entry.place(x = 7, y = 0)
+
+def create_window(x):
+	 _struct_Window = Tk()
+
+	 _struct_Window.geometry('500x500')
+
+	 _struct_Window.title(f'Diagram of {x}')
+
+	 _struct_Window.resizable(False, False)
+
+	 cid  = pubchem.get_cid(x)
+
+	 global my_image, _diagram
+
+
+	 pubchem.get_2d_photo(cid)
+
+
+	 if os.getcwd().split("/")[len(os.getcwd().split('/'))-1] != 'photos':
+	 	os.chdir('photos')
+
+
+
+	 if f'{cid}.gif' not in os.listdir():
+	 	time.sleep(5)
+
+
+	 my_image = PhotoImage(master = _struct_Window, file = f'{cid}.gif')
+
+	 _diagram = Label(master =_struct_Window, image = my_image)
+
+	 _diagram.pack()
+
+
