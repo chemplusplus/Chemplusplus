692d15b9225810d35236ad616dbfaeb5c78677aa
Makus_Frig <72632895+MakusFrig@users.noreply.github.com>
2021-08-18 19:45:53 -0700

    Update setup.py
    added support for font installation for linux

--- Code/setup.py
+++ Code/setup.py
@@ -14,3 +14,17 @@ os.system("pip3 install bs4")
 os.system("pip3 install urllib3")
 os.system("pip3 install Pillow")
 
+print("Checking fonts")
+
+
+if sys.platform == 'linux':
+	try: 
+		os.chdir("Assets/fonts")
+	except:
+		pass
+	for i in os.listdir():
+		if i not in os.listdir("/usr/local/share/fonts")
+			os.system(f"mv {i} /usr/local/share/fonts")
+			print(f"Installed Font {i.split(".")[0]}")
+	os.system("fc-cache -f -v")
+	print("Cache cleared")
